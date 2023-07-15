e29aff783a8bf664a9b23e5fc60edba75eea14f8
welerson <welerson_assis@hotmail.com>
2023-07-15 15:49:41 -0300

    error fixed

--- rotulador.py
+++ rotulador.py
@@ -1,5 +1,6 @@
 import re
 import logging
+import time
 from tqdm import tqdm
 import pandas as pd
 from thefuzz import fuzz
@@ -177,24 +178,23 @@ def has_number(string):
 
 
 if __name__ == '__main__':
-
     df = pd.read_excel('./base_proporcional_mercado_50000.xlsx')
     df_dict = df.to_dict('records')
     options = webdriver.ChromeOptions()
     options.add_argument('headless')
     driver = webdriver.Chrome(options=options)
     driver.get('https://buscacepinter.correios.com.br/app/endereco/index.php')
-    i = 0
+    
     for row in tqdm(df_dict):
-        i += 1
         score = 0
         mail_neighborhood_score = 0
         mail_street_score = 0
         try:
             # Input address
             input_zip = str(row['zip_code'])
-            input_street = unidecode(row['address']).upper()
-            input_street = pattern.sub(lambda match: street_replacements[match.group(0)], input_street).strip()
+            input_street = unidecode(str(row['address'])).upper()
+            input_street = pattern.sub(lambda match: street_replacements[match.group(0)], input_street)
+            input_street = input_street.strip()
             input_number = row['number']
             input_neighborhood = unidecode(str(row['quarter'])).upper()
             input_city = unidecode(row['city']).upper()
@@ -203,14 +203,15 @@ if __name__ == '__main__':
             zip_search = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.NAME, "endereco"))) 
             zip_search.send_keys(input_zip)
             zip_search.send_keys(Keys.ENTER)
-    
             # get mail address
             element = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'td[data-th="Logradouro/Nome"]'))) 
-            mail_street = unidecode(element.text).upper()
-            if '-' in mail_street:
+            mail_street_raw = unidecode(element.text).upper()
+            if '-' in mail_street_raw:
                 mail_street = mail_street.split('-')[0]
-            if ',' in mail_street:
+            if ',' in mail_street_raw:
                 mail_street = mail_street.split(',')[0]
+            else:
+                mail_street = mail_street_raw
             mail_street = pattern.sub(lambda match: street_replacements[match.group(0)], mail_street).strip()
             mail_street = mail_street.strip()
             element = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'td[data-th="Bairro/Distrito"]'))) 
@@ -221,7 +222,6 @@ if __name__ == '__main__':
             mail_state = unidecode(tmp.split('/')[1]).upper()
             element = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'td[data-th="CEP"]')))
             mail_zip = (element.text).replace('-','')
-
             if mail_street and mail_neighborhood and mail_city and mail_state and mail_zip: 
                 #ESTADO
                 if input_state == mail_state:
@@ -241,8 +241,7 @@ if __name__ == '__main__':
                 if input_zip[:5] == mail_zip[:5]:  
                     score += 20
 
-
-                row['CORREIOS_OUTPUT'] = mail_street+' , '+mail_neighborhood+' , '+mail_city+' , '+mail_state+' , '+mail_zip
+                row['CORREIOS_OUTPUT'] = mail_street_raw+' , '+mail_neighborhood+' , '+mail_city+' , '+mail_state+' , '+mail_zip
 
                 if score >= 50:
                     row['AUTO_CORREIOS'] = 'SIM'
@@ -262,8 +261,7 @@ if __name__ == '__main__':
                 if input_zip[:5] == mail_zip[:5]:  
                     score += 20
 
-
-                row['CORREIOS_OUTPUT'] = mail_street+' , '+mail_city+' , '+mail_state+' , '+mail_zip
+                row['CORREIOS_OUTPUT'] = mail_street_raw+' , '+mail_city+' , '+mail_state+' , '+mail_zip
 
                 if score == 50:
                     df_dict['AUTO_CORREIOS'] = 'SIM'
@@ -278,11 +276,9 @@ if __name__ == '__main__':
                 alert = element.text
                 if alert == 'Dados não encontrado':
                     row['AUTO_CORREIOS'] = 'NÃO'
-                else:
-                    logging.error(error)
-
                 new_search = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.ID, 'btn_nbusca')))
-        logging.info('Progresso: {} de 52676'.format(i))
+                new_search.click()
+  
 
-
+    df = pd.DataFrame(df_dict)
     df.to_excel('base_50k_rotulada.xlsx', index=False)
